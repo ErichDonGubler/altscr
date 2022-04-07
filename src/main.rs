458a9c9da77ae26f892d5c121d24c5dc928737d7
Erich Gubler <erichdongubler@gmail.com>
2022-04-07 18:44:24 -0400

    feat: add {no,print,silent} sub-options for `--pause`

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, bail, Context};
 use clap::Parser;
 use crossterm::{
     event::{self, Event},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{ffi::OsString, io::stdout, process::Command};
+use std::{ffi::OsString, io::stdout, process::Command, str::FromStr};
 
 #[derive(Debug, Parser)]
 struct Cli {
     #[clap(long)]
-    pause: bool,
+    pause: Option<Option<PauseOption>>,
     command: OsString,
     args: Vec<OsString>,
+}
+
+#[derive(Debug)]
+enum PauseOption {
+    No,
+    Silent,
+    Print,
+}
+
+impl FromStr for PauseOption {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "no" => Ok(Self::No),
+            "print" => Ok(Self::Print),
+            "silent" => Ok(Self::Silent),
+            unrecognized => bail!("unrecognized pause option {unrecognized:?}"),
+        }
+    }
+}
+
+impl PauseOption {
+    fn into_pause_config(self) -> Option<PauseConfig> {
+        match self {
+            PauseOption::No => None,
+            PauseOption::Silent => Some(PauseConfig { silent: true }),
+            PauseOption::Print => Some(PauseConfig { silent: false }),
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct PauseConfig {
+    silent: bool,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -21,6 +56,11 @@ fn main() -> anyhow::Result<()> {
         command,
         args,
     } = Cli::parse();
+
+    let pause = pause
+        .unwrap_or(Some(PauseOption::No))
+        .unwrap_or(PauseOption::Print)
+        .into_pause_config();
 
     execute!(stdout(), EnterAlternateScreen)
         .map_err(|e| anyhow!("failed to enter alternate screen: {}", e))?;
@@ -34,8 +74,10 @@ fn main() -> anyhow::Result<()> {
         .wait()
         .context("failed to get command's exit status")?;
 
-    if pause {
-        println!("dab: Press any key to exit alternate buffer...");
+    if let Some(PauseConfig { silent }) = pause {
+        if !silent {
+            println!("dab: Press any key to exit alternate buffer...");
+        }
         enable_raw_mode().map_err(|e| anyhow!("failed to enter raw mode: {e}"))?;
         loop {
             if let Event::Key(_key) =
