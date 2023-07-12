78d5de707601511230735cfd041c2252554acc23
Erich Gubler <erichdongubler@gmail.com>
2023-07-12 11:49:10 -0400

    WIP: feat!: use custom error repr. for lib.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-use anyhow::{anyhow, Context};
 use crossterm::{
     event::{self, Event},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use error::{
+    ExitAltScreenBufError, ExitRawModeAfterPauseError, PauseError, RunError, RunInAltScreenBufError,
+};
 use std::{
     ffi::{OsStr, OsString},
     io::stdout,
-    process::Command,
+    process::{Command, ExitStatus},
 };
 
 #[derive(Debug, Default)]
@@ -19,19 +21,57 @@ pub fn run_in_alt_screen_buf(
     command: &OsStr,
     args: &[OsString],
     pause: Option<PauseConfig>,
-) -> anyhow::Result<()> {
-    execute!(stdout(), EnterAlternateScreen)
-        // We can still bail here if this doesn't work, whew!
-        .map_err(|e| anyhow!("failed to enter alternate screen: {}", e))?;
+) -> RunInAltScreenBufOutcome {
+    match execute!(stdout(), EnterAlternateScreen)
+        .map_err(|source| RunInAltScreenBufError::EnterAltScreenBuf { source })
+    {
+        Ok(()) => (),
+        Err(e) => {
+            return RunInAltScreenBufOutcome {
+                run_res: Err(e),
+                exit_raw_mode_after_pause_err: Ok(()),
+                exit_alt_screen_err: Ok(()),
+            }
+        }
+    };
 
-    let run_res = run_and_pause(command, args, pause);
+    let RunAndPauseOutcome {
+        run_res,
+        exit_raw_mode_after_pause_err,
+    } = run_and_pause(command, args, pause);
+    let run_res = run_res.map_err(|source| RunInAltScreenBufError::Run { source });
 
-    if let Err(e) = execute!(stdout(), LeaveAlternateScreen) {
-        let e = anyhow!("warning: failed to exit alternate screen: {e}");
-        eprintln!("{e:#}");
+    let exit_alt_screen_err =
+        execute!(stdout(), LeaveAlternateScreen).map_err(|source| ExitAltScreenBufError { source });
+
+    RunInAltScreenBufOutcome {
+        run_res,
+        exit_raw_mode_after_pause_err,
+        exit_alt_screen_err,
     }
+}
 
-    run_res
+/// The outcome of calling [`run_in_alt_screen_buf`].
+#[must_use]
+#[derive(Debug)]
+pub struct RunInAltScreenBufOutcome {
+    /// Primary result of the operation. If this failed, then something prevented the execution of
+    /// the command specified for [`run_in_alt_screen_buf`].
+    pub run_res: Result<FinishedRunOutcome, RunInAltScreenBufError>,
+    /// An error in exiting the alternate buffer that may have been encountered while trying to
+    /// exit raw mode in the pause flow (after full command execution).
+    pub exit_raw_mode_after_pause_err: Result<(), ExitRawModeAfterPauseError>,
+    /// An error in exiting the alternate buffer that may have been encountered while exiting the
+    /// alternate screen buffer (after a full execution and pause flow).
+    pub exit_alt_screen_err: Result<(), ExitAltScreenBufError>,
+}
+
+/// The outcome of calling [`run_in_alt_screen_buf`].
+#[must_use]
+#[derive(Debug)]
+pub struct FinishedRunOutcome {
+    pub exit_status: ExitStatus,
+    pub pause_res: Result<(), PauseError>,
 }
 
 // TODO: preserve exit status
@@ -39,30 +79,180 @@ fn run_and_pause(
     command: &OsStr,
     args: &[OsString],
     pause: Option<PauseConfig>,
-) -> anyhow::Result<()> {
-    let mut child = Command::new(command)
+) -> RunAndPauseOutcome {
+    let mut exit_raw_mode_after_pause_err = Ok(());
+    let run_res = Command::new(command)
         .args(args)
         .spawn()
-        .context("failed to spawn command")?;
-
-    child
-        .wait()
-        .context("failed to get command's exit status")?;
-
-    if let Some(PauseConfig { silent }) = pause {
-        if !silent {
-            println!("dab: Press any key to exit alternate buffer...");
-        }
-        enable_raw_mode().map_err(|e| anyhow!("failed to enter raw mode: {e}"))?;
-        loop {
-            if let Event::Key(_key) =
-                event::read().map_err(|e| anyhow!("failed to read next input event: {e}"))?
-            {
-                break;
+        .map_err(|source| RunError::Spawn { source })
+        .and_then(|mut child| child.wait().map_err(|source| RunError::Wait { source }))
+        .map(|exit_status| {
+            let mut pause_res = Ok(());
+            if let Some(PauseConfig { silent }) = pause {
+                if !silent {
+                    println!("dab: Press any key to exit alternate buffer...");
+                }
+                pause_res = enable_raw_mode()
+                    .map_err(|source| PauseError::EnterRawMode { source })
+                    .and_then(|()| {
+                        loop {
+                            if let Event::Key(_key) = event::read()
+                                .map_err(|source| PauseError::GetInputEvent { source })?
+                            {
+                                break;
+                            }
+                        }
+                        Ok(())
+                    })
+                    .map(|res| {
+                        exit_raw_mode_after_pause_err = disable_raw_mode()
+                            .map_err(|source| ExitRawModeAfterPauseError { source });
+                        res
+                    });
             }
-        }
-        disable_raw_mode().map_err(|e| anyhow!("failed to exit raw mode: {e}"))?;
+            FinishedRunOutcome {
+                exit_status,
+                pause_res,
+            }
+        });
+
+    RunAndPauseOutcome {
+        run_res,
+        exit_raw_mode_after_pause_err,
+    }
+}
+
+#[derive(Debug)]
+struct RunAndPauseOutcome {
+    pub(crate) run_res: Result<FinishedRunOutcome, RunError>,
+    pub(crate) exit_raw_mode_after_pause_err: Result<(), ExitRawModeAfterPauseError>,
+}
+
+mod error {
+    use std::{
+        fmt::{self, Display, Formatter},
+        io,
+    };
+
+    /// An error encountered while running [`crate::run_in_alt_screen_buf`] that may interfere with
+    /// the run's correct execution, or a human's ability to evaluate it. Notably, it does _not_
+    /// include clean-up errors that may still be undesirable, but should not affect a human's
+    /// ability to determine such for themselves. For such error information, see
+    /// [`crate::RunInAltScreenBufOutcome`].
+    ///
+    /// This error representation does not implement [`Display`] because [`Self::Run`] may contain
+    /// multiple errors. Consuming code is encouraged to use the [`Self::description`] for standard
+    /// error descriptions of each variant in the error flow it uses.
+    #[derive(Debug)]
+    pub enum RunInAltScreenBufError {
+        EnterAltScreenBuf { source: io::Error },
+        Run { source: RunError },
     }
 
-    Ok(())
+    impl Display for RunInAltScreenBufError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let desc = match self {
+                Self::EnterAltScreenBuf { .. } => "failed to enter alternate screen",
+                Self::Run { .. } => "failed to run command",
+            };
+            write!(f, "{desc}")
+        }
+    }
+
+    impl std::error::Error for RunInAltScreenBufError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            Some(match self {
+                Self::EnterAltScreenBuf { source } => source,
+                Self::Run { source } => source,
+            })
+        }
+    }
+
+    /// An inner error source for [`Error::Run`]. You are strongly encouraged to handle
+    /// [`Self::Pause`], which may contain the exit status of the command that was run in
+    /// [`crate::run_in_alternate_screen`]
+    #[derive(Debug)]
+    pub enum RunError {
+        Spawn { source: io::Error },
+        Wait { source: io::Error },
+    }
+
+    impl Display for RunError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let desc = match self {
+                Self::Spawn { .. } => "failed to spawn command",
+                Self::Wait { .. } => "failed to get command's exit status",
+            };
+            write!(f, "{desc}")
+        }
+    }
+
+    impl std::error::Error for RunError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            Some(match self {
+                Self::Spawn { source } => source,
+                Self::Wait { source } => source,
+            })
+        }
+    }
+
+    /// Data associated with [`crate::RunInAltScreenBufOutcome`] `pause_res` field.
+    #[derive(Debug)]
+    pub enum PauseError {
+        EnterRawMode { source: io::Error },
+        GetInputEvent { source: io::Error },
+    }
+
+    impl Display for PauseError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let desc = match self {
+                Self::EnterRawMode { .. } => "failed to enter raw mode",
+                Self::GetInputEvent { .. } => "failed to read next input event",
+            };
+            write!(f, "{desc} in post-command pause flow")
+        }
+    }
+
+    impl std::error::Error for PauseError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            Some(match self {
+                Self::EnterRawMode { source } => source,
+                Self::GetInputEvent { source } => source,
+            })
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct ExitRawModeAfterPauseError {
+        pub(crate) source: io::Error,
+    }
+
+    impl Display for ExitRawModeAfterPauseError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "failed to exit raw mode in post-command pause flow")
+        }
+    }
+
+    impl std::error::Error for ExitRawModeAfterPauseError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            Some(&self.source)
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct ExitAltScreenBufError {
+        pub(crate) source: io::Error,
+    }
+
+    impl Display for ExitAltScreenBufError {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "failed to exit alternate screen")
+        }
+    }
+
+    impl std::error::Error for ExitAltScreenBufError {
+        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+            Some(&self.source)
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::{bail, Context};
 use clap::Parser;
-use dab::{run_in_alt_screen_buf, PauseConfig};
-use std::{ffi::OsString, str::FromStr};
+use dab::{run_in_alt_screen_buf, FinishedRunOutcome, PauseConfig, RunInAltScreenBufOutcome};
+use std::{ffi::OsString, process::ExitCode, str::FromStr};
 
 #[derive(Debug, Parser)]
 #[command(about, author, version)]
@@ -42,21 +42,83 @@ impl PauseOption {
     }
 }
 
-fn main() -> anyhow::Result<()> {
+fn main() -> ExitCode {
     let Cli {
         pause,
         command_and_args,
     } = Cli::parse();
 
-    let (command, args) = command_and_args
+    let (command, args) = match command_and_args
         .split_first()
-        .context("no command provided")?;
+        .context("no command provided")
+    {
+        Ok(tup) => tup,
+        Err(e) => {
+            eprintln!("{e:#}");
+            return ExitCode::from(255);
+        }
+    };
 
     let pause = pause
         .unwrap_or(Some(PauseOption::Print))
         .unwrap_or(PauseOption::Print)
         .into_pause_config();
 
-    run_in_alt_screen_buf(command, args, pause)
-        .context("failed to run command in alternate buffer")
+    let RunInAltScreenBufOutcome {
+        run_res,
+        exit_raw_mode_after_pause_err,
+        exit_alt_screen_err,
+    } = run_in_alt_screen_buf(command, args, pause);
+
+    let exit_code = match run_res {
+        Ok(finished) => {
+            let FinishedRunOutcome {
+                exit_status,
+                pause_res,
+            } = finished;
+            let exit_code = exit_status
+                .code()
+                .map(|_code| {
+                    // Don't use the code yet! Let's preserve this behavior until at least next
+                    // commit, this diff is already bad enough. :laugh:
+                    ExitCode::SUCCESS
+                })
+                .unwrap_or_else(|| {
+                    eprintln!("dab: child process was terminated by signal");
+                    ExitCode::from(253)
+                });
+            match pause_res {
+                Ok(()) => (),
+                Err(e) => {
+                    let e = anyhow::Error::new(e);
+                    eprintln!("dab: warning: succeeded in waiting for child process, but {e:#}");
+                }
+            }
+            exit_code
+        }
+        Err(e) => {
+            let e = anyhow::Error::new(e);
+            eprintln!("error: {e:#}");
+            return ExitCode::from(254);
+        }
+    };
+
+    match exit_raw_mode_after_pause_err {
+        Ok(()) => (),
+        Err(e) => {
+            let e = anyhow::Error::new(e);
+            eprintln!("warning: {e:#}");
+            return ExitCode::from(2);
+        }
+    }
+    match exit_alt_screen_err {
+        Ok(()) => (),
+        Err(e) => {
+            let e = anyhow::Error::new(e);
+            eprintln!("warning: {e:#}");
+            return ExitCode::from(2);
+        }
+    }
+
+    exit_code
 }
